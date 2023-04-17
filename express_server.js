0e905b81d01cea6d57d38611df80b68aaf3a0c88
DanRoss88 <danielmartinross@gmail.com>
2023-04-17 13:00:52 -0700

    updated shows

--- express_server.js
+++ express_server.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+app.set('view engine', 'ejs');
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-app.set('view engine', 'ejs');
+
 
 // index page
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.render('pages/index');
+});
+// route for urls
+app.get('/urls', (req, res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
+});
+// second route for url/id
+app.get('/urls/:id', (req, res) => {
+  const templateVars = {id: req.params.id, longURL : urlDatabase};
+  res.render("urls_show", templateVars);
 });
 
 app.get("/", (req, res) => {
