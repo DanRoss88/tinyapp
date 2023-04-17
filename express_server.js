e240dd689be2b9eb881093a8e431357fb431e8ec
DanRoss88 <danielmartinross@gmail.com>
2023-04-17 08:57:02 -0700

    changes to packages

--- express_server.js
+++ express_server.js
@@ -6,11 +6,23 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+app.set('view engine', 'ejs');
+
+// index page
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
